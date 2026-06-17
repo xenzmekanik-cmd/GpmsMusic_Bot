7943de229fe632c40a8bc1b98ce588bdda500b3f
xenzmekanik-cmd <xenzmekanik@gmail.com>
2026-06-17 22:35:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from pyrogram import Client, filters
-import os
+from pytgcalls import PyTgCalls
 from dotenv import load_dotenv
+import os
+import asyncio
 
 load_dotenv()
 
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+STRING_SESSION = os.getenv("STRING_SESSION")
+
 bot = Client(
     "bot",
-    api_id=int(os.getenv("API_ID")),
-    api_hash=os.getenv("API_HASH"),
-    bot_token=os.getenv("BOT_TOKEN")
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
+
+assistant = Client(
+    "assistant",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    session_string=STRING_SESSION
+)
+
+call = PyTgCalls(assistant)
+
 
 @bot.on_message(filters.command("start"))
 async def start(_, message):
@@ -17,9 +34,32 @@ async def start(_, message):
         "✅ Bot berhasil online"
     )
 
+
 @bot.on_message(filters.command("ping"))
 async def ping(_, message):
-    await message.reply_text("🏓 PONG")
+    await message.reply_text(
+        "🏓 PONG"
+    )
 
-print("BOT STARTED")
-bot.run()
+
+async def main():
+
+    print("STARTING BOT...")
+
+    await assistant.start()
+    print("ASSISTANT ONLINE")
+
+    await call.start()
+    print("PYTGCALLS ONLINE")
+
+    await bot.start()
+
+    me = await bot.get_me()
+
+    print(f"BOT ONLINE @{me.username}")
+
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
